daccc8c151a3c8d30b8cc0c3496c03c7ed440ff8
Kunyeul Lee <kunyeul18@gmail.com>
2025-12-17 21:33:09 -0800

    printing stats

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def main():
     response = client   .models.generate_content(
     model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
 )
+
+    if response.usage_metadata == None:
+        raise RuntimeError("API request failed... Please try again later.")
+    else:
+        usage = response.usage_metadata
+
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
     print(response.text)
     
 
