c51eba6a1f311540d265d022f5476f6b2ef5c1ad
Kunyeul Lee <kunyeul18@gmail.com>
2025-12-18 18:33:49 -0800

    add cmd arg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from dotenv import load_dotenv
 import os
 from google import genai
@@ -5,14 +6,19 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
+    parser = argparse.ArgumentParser(prog="Gemini prompt", description="Type your prompt.")
+    parser.add_argument("user_argument", type = str, help = "type your arguement")
+    args = parser.parse_args()
+
+
     
+
     if api_key == None:
         raise RuntimeError("API key not found")
     client = genai.Client(api_key=api_key)
 
     response = client   .models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    model='gemini-2.5-flash', contents=args.user_argument)
 
     if response.usage_metadata == None:
         raise RuntimeError("API request failed... Please try again later.")
